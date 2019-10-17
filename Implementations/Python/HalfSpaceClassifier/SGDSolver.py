ea3a0ccfed5e4341247eeb633fc3105803d4cb4c
Yuval Lavie <yuvallavie@gmail.com>
2019-10-17 17:14:11 +0300

    Update

--- Implementations/Python/HalfSpaceClassifier/SGDSolver.py
+++ Implementations/Python/HalfSpaceClassifier/SGDSolver.py
@@ -1,55 +1,47 @@
 import numpy as np
 
-def Hinge(X_train,Y_train,W):
-    loss = 0;
+# Helpers for surrogate optimization
+def g(X,W):
+    return (np.inner(X,W));
+    
+# Loss function
+
+def HingeLoss(X,W,y):
+    return np.maximum(0,1 - y*g(X,W))
+    
+def HingeDerivative(Y,X,W):
+    condition = 1 - Y * g(X,W);
+    if(condition > 0):
+        return -Y*X;
+    return 0;
+    
+def HingeCost(X_train,Y_train,W):
+    sum = 0;
     m = len(X_train);
     for i in range(m):
-        loss += np.maximum(0,1 - Y_train[i] * g(X_train[i],W));
-    return loss/m;
-  
-# Helpers for surrogate optimization
-def g(x,W):
-    return (np.inner(x,W));
-
-def Derivative(y,x,W,i):
-    condition = 1 - y * g(x,W);
-    if(condition <= 0):
-        return 0;
-    else:
-        return -y*x[i];
-      
+        #loss += np.maximum(0,1 - Y_train[i] * g(X_train[i],W));
+        sum += HingeLoss(X_train[i],W,Y_train[i]);
+    return sum/m;    
 
 #%%
 # ------------------------------------------------------------------------
-#                               Batch Gradient Descent
+#                       Stochastic Gradient Descent
+#                       batchSize = 1 -> Incremental Mode
+#                       btachSize = len(X) -> Gradient Descent
 # ------------------------------------------------------------------------
-def SGD(X,Y,objective,derivative,iterations,lRate,bSize):
+def SGD(X,Y,Obj,Der,T,lRate,batchSize):
+    # Add the psuedo-feature 1 for the bias calculation
     X = np.concatenate((X, np.ones((len(X),1))), axis=1);
     m = len(X[0]);
-    W = np.zeros(m);
-    for i in range(iterations):
-        # Print the function
-        currLoss = objective(X,Y,W);
-#        print("Objective:", currLoss);
-        if (currLoss == 0):
-               print("Gradient Descent optimization terminated after " + str(i) + " runs");
-               return W;
-        
-        # Initialize all gradients each run
-        subgradientW = np.zeros(m); 
-        # Batch Gradient Descent (Not sure if this is the stochastic equivalent)
-        # calculate gradients for each sample point in the batch
-        indices = np.random.randint(low=0,high=len(X),size=bSize);
-        for j in indices:
-            for k in range(m):
-                subgradientW[k] += derivative(Y[j],X[j],W,k);
-        # Add the gradients to the weights    
-        W -= lRate * subgradientW;
-        # Lower the learning rate
-        lRate = 0.99 * lRate;
-
-    print("Gradient Descent optimization Complete\nIterations: ",i);    
-    return W;
+    # Initialize the weights
+    W = np.zeros((T,m));
+    for t in range(T):
+        # Batch mode (use 1 for incremental, use len(X) for deterministic gradient descent)
+        batch = np.random.randint(-1,len(X),batchSize);
+        for j in batch:   
+            grad = Der(Y[j],X[j],W[t-1]);
+            W[t] = W[t-1] - lRate * grad;
+    return np.mean(W,axis=0);
 
 def Solve(X,Y,maxIter=1000,lRate=0.01,bSize=10):
-    return SGD(X,Y,Hinge,Derivative,maxIter,lRate,bSize);
+    return SGD(X,Y,HingeCost,HingeDerivative,maxIter,lRate,bSize);  
